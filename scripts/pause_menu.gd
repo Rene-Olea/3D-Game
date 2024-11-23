b29cc380864254e24576f8ca81b8d05e41f2e61e
Rene-Olea <reneolea11@gmail.com>
2024-11-22 21:47:41 -0500

    added pause menu

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,33 @@
+extends Control
+
+func _ready():
+	# Hide the pause menu initially
+	visible = false
+
+func resume():
+	visible = false  # Hide the pause menu
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)  # Lock the mouse back for gameplay
+
+func pause():
+	visible = true  # Show the pause menu
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)  # Show the mouse cursor for menu interaction
+
+func testEsc():
+	if Input.is_action_just_pressed("ui_cancel"):  # Use "ui_cancel" for Esc
+		if visible:
+			resume()
+		else:
+			pause()
+
+func _on_resume_pressed():
+	resume()
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+func _process(delta):
+	testEsc()
